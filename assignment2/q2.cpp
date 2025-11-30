e1d1a0b0b4bd10a9f85579e5e0f2e7344a321ca7
Aniket775 <araj2_be24@thapar.edu>
2025-12-01 03:32:29 +0530

    Create q2.cpp

---
+++ assignment2/q2.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubblesort(int arr[] , int n){
+	for(int i=0 ; i<n ; i++){
+		for(int j=i ; j<n-i-1 ; j++){
+			if(arr[j]> arr[j+1]){
+				swap(arr[j] , arr[j+1]);
+			}
+		}
+	}
+	
+	
+	cout<<"array after sorting:-"<<endl;
+	for(int i=0 ; i<n ; i++){
+		cout<<arr[i]<<" ";
+	}
+}
+
+
+int main(){
+	
+	int n;
+	cout<<"enter the size of array";
+	cin>>n;
+	int arr[n];
+	for(int i=0  ; i<n ; i++){
+		cin>>arr[i];
+	}
+	
+	bubblesort(arr , n);
+	
+	
+	
+	
+	return 0;
+}
