897b1a6b579a3a4b7d0128dbfde9e5b854612753
Aniket775 <araj2_be24@thapar.edu>
2025-12-01 03:49:02 +0530

    Create q4.cpp

---
+++ assignment5/q4.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+class LinkedList {
+    Node* head;
+public:
+    LinkedList() {
+        head = NULL;
+    }
+
+    void insertAtEnd(int val) {
+        Node* newNode = new Node(val);
+        if (head == NULL) {
+            head = newNode;
+            return;
+        }
+        Node* temp = head;
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = newNode;
+    }
+
+    void reverseList() {
+        Node* prev = NULL;
+        Node* curr = head;
+        Node* next = NULL;
+
+        while (curr != NULL) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        head = prev;
+        cout << "List has been reversed successfully." << endl;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != NULL) {
+            cout << temp->data;
+            if (temp->next != NULL) cout << "->";
+            temp = temp->next;
+        }
+        cout << "->NULL" << endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+    for (int i = 1; i <= 4; i++)
+        list.insertAtEnd(i);
+
+    cout << "Original List: ";
+    list.display();
+
+    list.reverseList();
+
+    cout << "Reversed List: ";
+    list.display();
+
+    return 0;
+}
+
+
+
+
